aeab97e5e7a9d3ba7700fb3793d5fbc8b83729ad
maxsaprykin <maxsaprykin@yandex.ru>
2018-03-15 18:00:53 +0300

    fix

--- hw5/hw5_t3.py
+++ hw5/hw5_t3.py
@@ -4,4 +4,8 @@ tree.fit(X, y)
 tree_grid = GridSearchCV (tree,tree_params,cv=skf,scoring='roc_auc')
 tree_grid.fit(X, y)
 
+# 1 вопрос
 tree_grid.best_score_
+
+# 2 вопрос
+tree_grid.cv_results_['std_test_score'][7]
