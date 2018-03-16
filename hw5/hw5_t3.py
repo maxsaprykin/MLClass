12b1b1a0d57728d2a15cd25b8aa23f7d8e67cce5
maxsaprykin <maxsaprykin@yandex.ru>
2018-03-16 17:38:14 +0300

    +  задания hw_5

--- hw5/hw5_t3.py
+++ hw5/hw5_t3.py
@@ -1,11 +1,8 @@
-tree = DecisionTreeClassifier(max_depth=3, random_state=17)
-tree.fit(X, y)
-
-tree_grid = GridSearchCV (tree,tree_params,cv=skf,scoring='roc_auc')
+tree_grid = GridSearchCV (dt,tree_params,cv=skf,scoring='roc_auc')
 tree_grid.fit(X, y)
 
 # 1 вопрос
 tree_grid.best_score_
 
 # 2 вопрос
-tree_grid.cv_results_['std_test_score'][7]
+tree_grid.cv_results_["std_test_score"][np.argmax(tree_grid.cv_results_["mean_test_score"])]*100
