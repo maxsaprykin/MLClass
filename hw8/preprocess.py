3524bb8595785535fa8d5e99c063e0ea13897662
maxsaprykin <maxsaprykin@yandex.ru>
2018-04-11 18:46:08 +0300

    реализовал препроцесс реализовал разбиение на 3 файла

--- hw8/preprocess.py
+++ hw8/preprocess.py
@@ -4,31 +4,43 @@ from tqdm import tqdm
 from time import time
 import numpy as np
 from sklearn.metrics import accuracy_score
+import mmap
 
 input_filename = sys.argv[1]
 output_filename = sys.argv[2]
 
-input_file = open(input_filename, 'r')
 output_file = open(output_filename, 'a')
 
 #теги классов, на которые мы будем классифицировать весь текст
 target_tags = ['javascript', 'java', 'python', 'ruby', 'php', 'c++', 'c#', 'go', 'scala', 'swift']
 
-for line in tqdm(input_file):
-	if (line.count('\t') == 0) or (line.count('\t') > 1):
-		continue
-	else:		
-		splited_line = line.split('\t')
-		text = splited_line[0]
-		tags = splited_line[1]
-		tags = tags.split('\s')
-		is_more_than_once = False
-		for tag in tags:
-			is_more_than_once = True if (target_tags.count(tag) > 1) or (target_tags.count(tag) < 1)
-		if (is_more_than_once == True):
+with open(input_filename) as file:
+	for line in tqdm(file, total = 10000000):
+		line = line.strip() 	
+		if (line.count('\t') == 0) or (line.count('\t') > 1):
 			continue
 		else:
-			text.replace('?','')
-			text.replace(':','')
-			text.replace('|','')
-			new_line = str(target_tags.index(tags[0])) + ' | ' + str(text) + '\n'
+			splited_line = line.split('\t')
+			striped_line = line.strip()
+			text = striped_line[0]
+			tags = striped_line[1]
+			tags = tags.replace('\n', '')
+			tags = tags.split(' ')
+			tags_counter = 0
+			new_tags = []           
+			for tag in tags:
+				if (tag in target_tags):
+					new_tags.append(tag)
+				tags = new_tags
+			for tag in target_tags:
+				if (tag in tags):
+					tags_counter = tags_counter + 1
+			if (tags_counter != 1):
+				continue
+			else:
+				text = text.replace('?','')
+				text = text.replace(':','')
+				text = text.replace('|','')
+				new_line = str(int(target_tags.index(tags[0])) + 1) + ' | ' + str(text) + '\n' 
+				output_file.write(new_line)
+output_file.close()
